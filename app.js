8d9efc99981afd8d207f9efbf7750de6f2ae7636
ray <30444958+rayli24@users.noreply.github.com>
2021-05-25 02:33:07 -0300

    init

---
+++ app.js
@@ -0,0 +1,11 @@
+const app = Vue.createApp({
+    data(){
+        return {
+            firstName: 'Raylison',
+            lastName: 'Ortela',
+            picture: 'img/graduation.jpg',
+        }
+    }
+})
+
+app.mount('#app');
